7fa26fa3eecbdab6437d2caba64360c3b9aa311c
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-11-01 16:35:22 -0300

    feat: :sparkles: update basic script with release_date

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const hltb = require('howlongtobeat')
 const hltbService = new hltb.HowLongToBeatService()
-const axiosWebApi = require('./axios.js')
+const { steamWebApi, steamStoreApi } = require('./axios.js')
 
-axiosWebApi.getOwnedGames().then(async result => {
+steamWebApi.getOwnedGames().then(async result => {
   const games = result.data.response.games
-  console.log('game;time')
+  console.log('game;time;release_date')
   games.forEach(async game => {
-    await hltbService.search(game.name).then(result => {
+    await hltbService.search(game.name).then(async result => {
       if (result[0].gameplayMain === 0) {
         result[0].gameplayMain = 'less than an hour or not registered'
       }
-      console.log(result[0].name + ';' + result[0].gameplayMain)
+      const name = game.name
+      const time = result[0].gameplayMain
+
+      const releaseDate = await steamStoreApi.getGameDetails(game.appid).then(result => {
+        return result.data[game.appid].data.release_date.date
+      }).catch(_err => {
+        return 'error'
+      })
+
+      console.log(name + ';' + time + ';' + releaseDate)
     }).catch(_err => {})
   })
 }).catch(_err => {})
